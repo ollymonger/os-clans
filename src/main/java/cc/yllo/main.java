cca30580372f6169cb420abd5216639b328c83fe
Olly Monger <olivermonger123@gmail.com>
2022-08-23 17:27:07 +0100

    [Feat] clan in game list (#5)
    * update readme
    
    * Add ability to list all clans, and get more information when searching for a specific clan
    
    * add todo comment
    
    * remove unused
    
    * rename > info
    
    * Update list formatting
    
    * Update formatting

--- src/main/java/cc/yllo/main.java
+++ src/main/java/cc/yllo/main.java
@@ -54,16 +54,3 @@ public class main extends JavaPlugin {
         }
     }
 }
-
-
-/*// This code is called after the server starts and after the /reload command
-    @Override
-    public void onEnable() {
-        getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
-    }
-
-    // This code is called before the server stops and after the /reload command
-    @Override
-    public void onDisable() {
-        getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
-    } */
